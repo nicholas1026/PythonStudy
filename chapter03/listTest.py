3cfb52728c6a8d680a41a0af671a578cbd2d4ff4
nicholas1026 <xiyunfei1026@163.com>
2018-06-11 18:30:29 +0800

    111

--- chapter03/listTest.py
+++ chapter03/listTest.py
@@ -4,9 +4,9 @@ print(names[0])
 print(names[1])
 print(names[2])
 
-message="Hello,"+names[0]+"what are you doing?"
+message="Hello,"+names[0]+" what are you doing?"
 print(message)
-message="Hello,"+names[1]+"what are you doing?"
+message="Hello,"+names[1]+" what are you doing?"
 print(message)
-message="Hello,"+names[2]+"what are you doing?"
+message="Hello,"+names[2]+" what are you doing?"
 print(message)
