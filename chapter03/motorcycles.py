fea823de0b306526d160d3414d12b3a01f50c8c6
Nicholas1026 <xiyunfei@xiyunfeideMacBook-Pro.local>
2018-06-13 12:41:41 +0800

    111

---
+++ chapter03/motorcycles.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+motorycycles = ['honda','yamaha','suzuki']
+
+print(motorycycles)
+
+motorycycles[0] = 'ducati'
+
+print(motorycycles)
+
+motorycycles.append('cherry')
+
+print(motorycycles)
+
+motorycycles.insert(3,'audi')
+
+print(motorycycles)
+
+del motorycycles[2]
+
+print(motorycycles)
+
+popped_motorcycles = motorycycles.pop()
+print(motorycycles)
+print(popped_motorcycles)
+
+motorycycles.pop(0)
+print(motorycycles)
+
+motorycycles = ['honda','yamaha','suzuki']
+motorycycles.remove('yamaha')
+print(motorycycles)
