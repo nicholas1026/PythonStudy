840520ef7c9bc93dd8466c0bdf9b45c963809ae1
nicholas1026 <xiyunfei1026@163.com>
2018-06-13 15:30:17 +0800

    111

--- chapter02/name.py
+++
@@ -1,9 +0,0 @@
-name="ada lovelace"
-print(name.title())
-print(name.upper())
-print(name.lower())
-
-myName=' yunfei xi '
-print("["+name.lstrip()+"]")
-print("["+name.rstrip()+"]")
-print("["+name.strip()+"]")
