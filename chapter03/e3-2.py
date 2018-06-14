b59318dae5f2fea08d01631932c118d17d9de58f
nicholas1026 <xiyunfei1026@163.com>
2018-06-14 16:18:55 +0800

    12

--- chapter03/e3-2.py
+++ chapter03/e3-2.py
@@ -9,3 +9,27 @@ new_person="fanbingbing"
 invite_list[1]=new_person
 
 print(invite_list)
+
+invite_list.insert(0,"afei")
+print(invite_list)
+
+invite_list.insert(2,"yuanyuan")
+print(invite_list)
+
+invite_list.append("choubao")
+print(invite_list)
+
+f=invite_list.pop()
+print(f)
+s=invite_list.pop()
+print(s)
+t=invite_list.pop()
+print(t)
+fo=invite_list.pop()
+print(fo)
+print(invite_list)
+
+del invite_list[0]
+print(invite_list)
+del invite_list[0]
+print(invite_list)
